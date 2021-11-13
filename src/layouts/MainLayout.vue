7bfcb4956d37bab37f050b09afa717308604cea1
TheDream <thedreamofbeast@hotmail.com>
2021-11-13 12:46:30 -0500

    Layout base

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,220 @@
+<template>
+    <q-layout view="hHh lpR fFf" class="bg-grey-1">
+        <q-header elevated class="bg-white text-grey-8" height-hint="64">
+            <q-toolbar class="GNL__toolbar">
+                <q-btn
+                    flat
+                    dense
+                    round
+                    @click="toggleLeftDrawer"
+                    aria-label="Menu"
+                    icon="menu"
+                    class="q-mr-sm"
+                />
+
+                <q-toolbar-title
+                    v-if="$q.screen.gt.xs"
+                    shrink
+                    class="row items-center no-wrap"
+                >
+                    <!-- <img
+                        src="https://cdn.quasar.dev/img/layout-gallery/logo-google.svg"
+                    /> -->
+                    <span class="q-ml-sm">Red social</span>
+                </q-toolbar-title>
+
+                <q-space />
+
+                <q-input
+                    class="GNL__toolbar-input"
+                    outlined
+                    dense
+                    v-model="search"
+                    color="bg-grey-7 shadow-1"
+                    placeholder="Search people"
+                >
+                    <template v-slot:prepend>
+                        <q-icon v-if="search === ''" name="search" />
+                        <q-icon
+                            v-else
+                            name="clear"
+                            class="cursor-pointer"
+                            @click="search = ''"
+                        />
+                    </template>
+                </q-input>
+
+                <q-space />
+
+                <div class="q-gutter-sm row items-center no-wrap">
+                    <!-- <q-btn
+                        v-if="$q.screen.gt.sm"
+                        round
+                        dense
+                        flat
+                        color="text-grey-7"
+                        icon="apps"
+                    >
+                        <q-tooltip>Google Apps</q-tooltip>
+                    </q-btn>
+                    <q-btn round dense flat color="grey-8" icon="notifications">
+                        <q-badge color="red" text-color="white" floating>
+                            2
+                        </q-badge>
+                        <q-tooltip>Notifications</q-tooltip>
+                    </q-btn> -->
+                    <q-btn round flat>
+                        <q-avatar size="26px">
+                            <img
+                                src="https://cdn.quasar.dev/img/boy-avatar.png"
+                            />
+                        </q-avatar>
+                        <q-tooltip>Account</q-tooltip>
+                    </q-btn>
+                </div>
+            </q-toolbar>
+        </q-header>
+
+        <q-drawer
+            v-model="leftDrawerOpen"
+            show-if-above
+            bordered
+            class="bg-white"
+            :width="280"
+        >
+            <q-scroll-area class="fit">
+                <q-list padding class="text-grey-8">
+                    <q-item
+                        class="GNL__drawer-item"
+                        v-ripple
+                        v-for="link in links1"
+                        :key="link.text"
+                        clickable
+                    >
+                        <q-item-section avatar>
+                            <q-icon :name="link.icon" />
+                        </q-item-section>
+                        <q-item-section>
+                            <q-item-label>{{ link.text }}</q-item-label>
+                        </q-item-section>
+                    </q-item>
+
+                    <q-separator inset class="q-my-sm" />
+
+                    <div class="q-mt-md">
+                        <div class="flex flex-center q-gutter-xs">
+                            <a
+                                class="GNL__drawer-footer-link"
+                                href="javascript:void(0)"
+                                aria-label="About"
+                                >2 days free!!</a
+                            >
+                        </div>
+                    </div>
+                </q-list>
+            </q-scroll-area>
+        </q-drawer>
+
+        <q-page-container>
+            <router-view />
+        </q-page-container>
+    </q-layout>
+</template>
+
+<script lang="ts">
+import { ref } from 'vue';
+// import { fasGlobeAmericas, fasFlask } from '@quasar/extras/fontawesome-v5';
+
+export default {
+    name: 'MainLayout',
+
+    setup() {
+        const leftDrawerOpen = ref(false);
+        const search = ref('');
+        const showAdvanced = ref(false);
+        const showDateOptions = ref(false);
+        const exactPhrase = ref('');
+        const hasWords = ref('');
+        const excludeWords = ref('');
+        const byWebsite = ref('');
+        const byDate = ref('Any time');
+
+        function onClear() {
+            exactPhrase.value = '';
+            hasWords.value = '';
+            excludeWords.value = '';
+            byWebsite.value = '';
+            byDate.value = 'Any time';
+        }
+
+        function changeDate(option: string) {
+            byDate.value = option;
+            showDateOptions.value = false;
+        }
+
+        function toggleLeftDrawer() {
+            leftDrawerOpen.value = !leftDrawerOpen.value;
+        }
+
+        return {
+            leftDrawerOpen,
+            search,
+            showAdvanced,
+            showDateOptions,
+            exactPhrase,
+            hasWords,
+            excludeWords,
+            byWebsite,
+            byDate,
+
+            links1: [
+                { icon: 'web', text: 'Publications' },
+                { icon: 'person', text: 'People' },
+            ],
+
+            onClear,
+            changeDate,
+            toggleLeftDrawer,
+        };
+    },
+};
+</script>
+
+<style lang="scss">
+.GNL {
+    &__toolbar {
+        height: 64px;
+    }
+    &__toolbar-input {
+        width: 55%;
+    }
+    &__drawer-item {
+        line-height: 24px;
+        border-radius: 0 24px 24px 0;
+        margin-right: 12px;
+
+        .q-item__section--avatar {
+            .q-icon {
+                color: #5f6368;
+            }
+        }
+        .q-item__label {
+            color: #3c4043;
+            letter-spacing: 0.01785714em;
+            font-size: 0.875rem;
+            font-weight: 500;
+            line-height: 1.25rem;
+        }
+    }
+    &__drawer-footer-link {
+        color: inherit;
+        text-decoration: none;
+        font-weight: 500;
+        font-size: 0.75rem;
+
+        &:hover {
+            color: #000;
+        }
+    }
+}
+</style>
